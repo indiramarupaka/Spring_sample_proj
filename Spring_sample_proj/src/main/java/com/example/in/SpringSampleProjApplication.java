5bc84c2bfd3706dee52c2a8e9d9d8ea9b4c9aee4
indira <indira.marpaka@gmail.com>
2024-08-21 11:29:40 +0530

    change added'

---
+++ Spring_sample_proj/src/main/java/com/example/in/SpringSampleProjApplication.java
@@ -0,0 +1,16 @@
+package com.example.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleProjApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringSampleProjApplication.class, args);
+		System.out.println("Hello");
+		
+	}
+
+}
---
+++ Spring_sample_proj/src/test/java/com/example/Spring_sample_proj/SpringSampleProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_sample_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
